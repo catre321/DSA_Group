9a938fa3bf0ae81c3c2712d110e8809a50222acc
Vu Bui Khanh Linh <73817943+Linh-0v0@users.noreply.github.com>
2023-12-08 13:32:11 +0700

    Update SecretKeyGuesser.java

--- SecretKeyGuesser.java
+++ SecretKeyGuesser.java
@@ -15,29 +15,25 @@ public class SecretKeyGuesser {
     }
 
     private void guessRecursive(char[] current, int index) {
-        if (index == 12) {
-            // Base case: the key when it reaches 12 characters
-            System.out.println("Number of guesses (tracking): " + key.getCounter());
-            System.out.println("I found the secret key. It is " + new String(current));
-            return;
-        }
+        System.out.println("Number of guesses (tracking): " + key.getCounter());  
+        System.out.println("I found the secret key. It is " + new String(current));
 
         char[] candidates = { 'M', 'O', 'C', 'H', 'A' };
         for (char candidate : candidates) {
+            // Break if the correct key is found
+            if (matched == 12) {
+                break;
+            }
             current[index] = candidate;
             // Check if the guess is valid
             matched = key.guess(new String(current));
 
-            if (matched != -1 && matched >= index) {
-                // `matched >= index` (additional pruning condition): Only proceed to the char
-                // if the number of matched is at least the current index
+            if (matched != -1 && matched > index) {
+                // `matched > index` (additional pruning condition): Only proceed to the char
+                // if the number of `matched` is larger than the `current index`
                 // If the number of matched characters so far is less than the current index,
                 // skip unnecessary recursive calls.
                 guessRecursive(current, index + 1);
-            }
-            // Break if the correct key is found
-            if (matched == 12) {
-                break;
             }
         }
     }
